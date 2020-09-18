746a1d2257e7ca45c475fd671024a563560fcfe1
Lyndon Tan <pikadon92@gmail.com>
2020-09-18 22:36:44 +0800

    Main Screen

--- addons/fsm-node/plugin.gd
+++ addons/fsm-node/plugin.gd
@@ -1,14 +1,27 @@
 tool
 extends EditorPlugin
 
+var MainPanel = preload("main_screen/MainPanel.tscn")
+var main_panel_instance
+
 func _enter_tree():
-	pass
+	main_panel_instance = MainPanel.instance()
+	get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
+	make_visible(false)
 
 func _exit_tree():
-	pass
+	if main_panel_instance:
+		main_panel_instance.queue_free()
 
 func get_plugin_icon():
-	return preload("main_scene/icon.svg")
+	return preload("main_screen/icon.svg")
 
 func get_plugin_name():
 	return "FSM"
+
+func has_main_screen():
+	return true
+	
+func make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
