d927a72209a3912b68d2529223d669cf8f1b63b8
mechPenSketch <pikadon92@gmail.com>
2021-06-18 18:44:30 +0800

    When Moused Over Either Side

--- addons/fsm-node/main_screen/graph_nodes/ComponentGraphNode.gd
+++ addons/fsm-node/main_screen/graph_nodes/ComponentGraphNode.gd
@@ -3,6 +3,20 @@ extends GraphNode
 
 var associated_component:Node
 
+func _gui_input(e):
+	
+	if e is InputEventMouse:
+		# GET MOUSE POSITION X
+		var mouse_x = e.get_position().x
+		
+		# GET WIDTH
+		var width = get_size().x
+		
+		if mouse_x <= width / 4:
+			print("Left")
+		elif mouse_x >= width * 0.75:
+			print("Right")
+
 func _on_dragged(_from, to):
 	associated_component.graph_offset = to
 
