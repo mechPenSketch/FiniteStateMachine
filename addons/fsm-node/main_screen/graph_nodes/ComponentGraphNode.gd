358fd4da2d99827af6e5ae4561af93fb95e2e6d9
mechPenSketch <pikadon92@gmail.com>
2021-06-20 16:09:57 +0800

    Upon Clicking on the Edges

--- addons/fsm-node/main_screen/graph_nodes/ComponentGraphNode.gd
+++ addons/fsm-node/main_screen/graph_nodes/ComponentGraphNode.gd
@@ -3,19 +3,28 @@ extends GraphNode
 
 var associated_component:Node
 
+var is_connectable = true
+
+export(StyleBox) var hover_left
+
 func _gui_input(e):
 	
-	if e is InputEventMouse:
-		# GET MOUSE POSITION X
-		var mouse_x = e.get_position().x
-		
-		# GET WIDTH
-		var width = get_size().x
-		
-		if mouse_x <= width / 4:
-			print("Left")
-		elif mouse_x >= width * 0.75:
-			print("Right")
+	if is_connectable:
+		if e is InputEventMouse:
+			# GET MOUSE POSITION X
+			var mouse_x = e.get_position().x
+			
+			# GET WIDTH
+			var width = get_size().x
+			
+			if mouse_x <= width / 4:
+				print("Left")
+				if e is InputEventMouseButton and e.is_pressed():
+					print("connect from...")
+			elif mouse_x >= width * 0.75:
+				print("Right")
+				if e is InputEventMouseButton and e.is_pressed():
+					print("connect to...")
 
 func _on_dragged(_from, to):
 	associated_component.graph_offset = to
