f028a70689058a505add899edb962ce5ad05f717
mechPenSketch <pikadon92@gmail.com>
2023-07-21 15:30:35 +0800

    Update transition.gd

--- addons/fsm-node/nodes/transition.gd
+++ addons/fsm-node/nodes/transition.gd
@@ -12,8 +12,10 @@ var incoming_signals
 func _condition():
 	get_parent().change_state(target_state_node)
 
+
 func _get_configuration_warning():
 	return "" if get_parent().is_class("FSM") else "Parent should be FSM."
+
 
 func _ready():
 	super._ready()
@@ -26,13 +28,15 @@ func _ready():
 		incoming_signals = get_incoming_connections()
 		set_active(false)
 
+
 func get_class():
 	return "Transition"
+
 
 func set_active(b:bool):
 	if b:
 		for d in incoming_signals:
-			d["source"].connect(d["signal_name"], self, d["method_name"])
+			d["signal"].connect(d["callable"])
 	else:
 		for d in incoming_signals:
-			d["source"].disconnect(d["signal_name"], self, d["method_name"])
+			d["signal"].disconnect(d["callable"])
